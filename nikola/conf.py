17ba7bf71cabba61d500bdf132059db9431d5393
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2018-07-21 00:03:23 +0200

    Clone the jidn theme to bza-jidn for customization.

--- nikola/conf.py
+++ nikola/conf.py
@@ -154,7 +154,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "jidn"
+THEME = "bza-jidn"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
